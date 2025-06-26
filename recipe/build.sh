a344efa77ca2da095c8e997fd3e36dfa5cf2ae91
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2025-06-26 14:18:36 +0200

    assimp 6.0.2  (#4)
    * PKG-8515 bump version to 6.0.2
    
    * run upstream tests
    
    * fix test path on Windows
    
    * skip float precision tests
    
    * use custom test on Win64

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,10 +4,15 @@ mkdir build && cd build
 
 cmake ${CMAKE_ARGS} .. \
   -DASSIMP_BUILD_ASSIMP_TOOLS=OFF \
-  -DASSIMP_BUILD_TESTS=OFF \
+  -DASSIMP_BUILD_TESTS=ON \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_BUILD_TYPE=Release \
   -DASSIMP_WARNINGS_AS_ERRORS:BOOL=OFF
 
 make -j${CPU_COUNT}
+
+# Run upstream tests as part of the build due to tests being tightly coupled to the main
+# library code and build process. Skip floating precision tests.
+bin/unit --gtest_filter=-AssimpAPITest_aiMatrix3x3.aiMatrix3FromToTest:AssimpAPITest_aiMatrix4x4.aiMatrix4FromToTest:AssimpAPITest_aiQuaternion.aiQuaternionFromNormalizedQuaternionTest
+
 make install
